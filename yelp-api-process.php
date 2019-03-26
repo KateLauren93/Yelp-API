191129468d4e758235c25e767102b1a02ad504ec
Katie Carson <kate-carson@outlook.com>
2019-03-25 23:47:15 -0400

    Working on php loop

--- yelp-api-process.php
+++ yelp-api-process.php
@@ -4,14 +4,13 @@
 $curl = curl_init();
 
 // get the search variables
-// $searchReq = $_GET['search'];
-// $locationReq = $_GET['location'];
+$searchReq = $_GET['search'];
+$locationReq = $_GET['location'];
 
 // set some options
 curl_setopt_array($curl, [
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'https://api.yelp.com/v3/businesses/search?term=delis&location=toronto', // hard coded
-    // CURLOPT_URL => 'https://api.yelp.com/v3/businesses/search?term=' . $searchReq . '&location=' . $locationReq,
+    CURLOPT_URL => CURLOPT_URL => 'https://api.yelp.com/v3/businesses/search?term=' . $searchReq . '&location=' . $locationReq,
     CURLOPT_HTTPHEADER => ['Authorization: Bearer DnUxmqxm6SaNnppYmdzWww8XKYmXLI03MYhNNC1Bk9agm9YuOKrW_8p6G6tm9_zEmsXspQ8QahVpZ-Ad0xTr6Hs1UnGn7FqP--gRujjIp501KzPpAEpPzhJ5fxyJXHYx']
 ]);
 
@@ -24,21 +23,22 @@ curl_close($curl);
 // print all responses
 // var_dump($resp->businesses);
 
-// print the data that you want
+// print certain responses
 // echo($resp->businesses[0]->name);
 
-///////////////////////////////////////////////////
+?>
 
-// playing around with loops, can't figure them out
+<!-- php loop to display results -->
+<div id="results" data-url="<?php if (!empty($url)) echo $url ?>">
+    <?php
+    if (!empty($array)) {
+        foreach ($array['businesses'] as $key => $item) {
+            echo '<img id="' . $item['id'] . '" src="' . $item['image_url'] . '" alt=""/><br/>';
+        }
+    }
+    ?>
+</div>
 
-// foreach ($resp->businesses as $item) {
-//     var_dump();
-// }
-
-foreach($resp as $i => $item) {
-    echo $resp[$i]['name'];
-    echo $resp[$i]['rating'];
-    // $resp[$i] is same as $item
-};
+<?php
 
 ?>
